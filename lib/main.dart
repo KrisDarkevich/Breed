4bf3165f5bb0db1458638ab52a379f27d22f8871
Kseniya Boldyreva <KristinaDarkevich@mail.ru>
2024-04-02 19:50:07 +0300

    added interceptor

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,26 @@
-import 'dart:io';
+// import 'dart:io';
 
 import 'package:cat_breeds/logic/api_call.dart';
 import 'package:cat_breeds/logic/my_app.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
-  final client = HttpClient();
+  final client = Dio()
+    ..interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          print('ti lox2');
+
+          return handler.next(options);
+        },
+        onResponse: (response, handler) {
+          print('ti lox');
+
+          return handler.next(response);
+        },
+      ),
+    );
   final api = ApiClient(client);
   final response = await api.getUrl();
   runApp(
